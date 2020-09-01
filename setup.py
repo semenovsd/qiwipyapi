59bdec88be535c733717ded501355633841d8d73
Semenovsd <semenov_sd@bk.ru>
2020-09-01 18:24:41 +0300

    Add License

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
 setup(name='qiwipyapi',
-      version='0.0',
+      keywords='qiwipyapi',
+      version='0.01',
       description='Lib for easy work with QIWI Wallet API и QIWI P2P API',
+      url='https://github.com/semenovsd/qiwipyapi',
       packages=['qiwipyapi'],
+      author='Stanislav Semenov',
       author_email='semenov_sd@bk.ru',
+      license="MIT",
+      # install_requires=requirements,
+      long_description='Lib for easy work with QIWI Wallet API и QIWI P2P API',
       zip_safe=False)
