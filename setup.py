e105e4026807f0c671dd42b4088ea2751b8d366e
Semenovsd <semenov_sd@bk.ru>
2020-09-01 20:05:17 +0300

    Refact

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(name='qiwipyapi',
-      keywords='qiwipyapi',
-      version='0.01',
-      description='Lib for easy work with QIWI Wallet API и QIWI P2P API',
-      url='https://github.com/semenovsd/qiwipyapi',
-      packages=['qiwipyapi'],
-      author='Stanislav Semenov',
-      author_email='semenov_sd@bk.ru',
-      license="MIT",
-      # install_requires=requirements,
-      long_description='Lib for easy work with QIWI Wallet API и QIWI P2P API',
-      zip_safe=False)
+setup(
+    name='qiwipyapi',
+    keywords='qiwipyapi',
+    version='0.01',
+    packages=['qiwipyapi'],
+    url='https://github.com/semenovsd/qiwipyapi',
+    license='MIT',
+    author='Stanislav Semenov',
+    author_email='semenov_sd@bk.ru',
+    description='Lib for simple work with QIWI Wallet API and/or QIWI P2P API'
+)
